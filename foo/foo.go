a347cfc17fe480805ef36026c66bafd075ade552
Dhia Ayachi <dhia@hashicorp.com>
2023-02-08 09:41:38 -0500

    add foo

---
+++ foo/foo.go
@@ -0,0 +1,23 @@
+package foo
+
+import "fmt"
+
+type Foo interface {
+	Name() string
+	License() string
+}
+
+type OssFoo struct {
+}
+
+func (o OssFoo) Name() string {
+	return "Foo"
+}
+
+func (o OssFoo) License() string {
+	return "OSS"
+}
+
+func PrintFoo(f Foo) {
+	fmt.Printf("%s:%s\n", f.Name(), f.License())
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dhiaayachi/foo-oss/foo"
+)
+
+func main() {
+	foo.PrintFoo(&foo.OssFoo{})
+}
